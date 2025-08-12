ecc8d58866005f1540b603c477362e4e7b42e02a
Brian Kalbermatter <tejant6@hotmail.com>
2025-08-12 08:54:24 -0300

    CLI

---
+++ compiler.pas
@@ -0,0 +1,44 @@
+// ACCION compiler ES
+program compiler;
+
+
+
+
+
+
+
+
+// AMBIENTE
+var
+inicio: integer;
+
+
+
+
+
+
+
+
+
+
+
+// FUNCIONES Y PROCEDIMIENTOS
+
+
+
+
+
+
+
+
+
+
+
+// PROCESO
+begin
+    ReadLn(inicio);
+    WriteLn('Hola, Bienvenido');
+    
+    // BUCLES
+
+end.
